ef15a13af6d7549c2ccccf07deeada343af690f4
Kania Lywe <kanialywe@gmail.com>
2020-03-09 16:25:28 -0700

    need to deal with outlier

--- co2_emission_kania2.R
+++ co2_emission_kania2.R
@@ -1,8 +1,8 @@
 co2_emi_sample <- read.csv("C:/Users/galz_/Desktop/DANA 4801/project/CO2_emission_vehicle/data/Sample_CO2Emission.csv")
 attach(co2_emi_sample)
 
-#print all output
-options(print.max=10000000000)
+library(corpcor)
+cor2pcor(cov(X))
 
 #create data frame
 df<- data.frame(co2_emi_sample)
@@ -15,6 +15,16 @@ df<- df %>% rename(fuel.consumption.hwy = Fuel.Consumption..Hwy..L.100.km.,
                    fuel.consumption.city = Fuel.Consumption..City..L.100.km.,
                    fuel.consumption.comb.mpg = Fuel.Consuption..Comb..mpg.)
 
+#get all possible regression model by all-possible-regression selection 
+library(olsrr)
+fit.var.sel <- lm(co2.emissions~ Vehicle.Class+Transmission+engine.size+Fuel.Type+fuel.consumption.comb+Cylinders, data = df)
+summary(fit.var.sel)
+reg.combi<- ols_step_all_possible(fit.var.sel)
+
+#plot re.combi results: R, R-squared, Cp, AIC, SBC, SBIC
+#from graphs (baased on Cp, AIC,, SBC, SBIC): 3 variables- Vehicle.Class Fuel.Type fuel.consumption.comb
+plot(reg.combi) 
+
 #plot each variables with co2 emissions (y)
 for (i in 1:length(colnames(df))){
   if (colnames(df)[i] != "co2.emissions"){
@@ -22,97 +32,102 @@ for (i in 1:length(colnames(df))){
   }
 }
 
-
 #fuel type, 4 levels; 3 dummy variable
 #diesel,D, as baseline
-ethanol <- rep(0,length(df$Fuel.Type)) #E
-ethanol[df$Fuel.Type == "E"]<-1
+df$ethanol <- rep(0,length(df$Fuel.Type)) #E
+df$ethanol[df$Fuel.Type == "E"]<-1
 
-reg.gas<- rep(0,length(df$Fuel.Type)) #X
-reg.gas[df$Fuel.Type == "X"]<-1
+df$reg.gas<- rep(0,length(df$Fuel.Type)) #X
+df$reg.gas[df$Fuel.Type == "X"]<-1
 
-prem.gas<- rep(0,length(df$Fuel.Type)) #Z
-prem.gas[df$Fuel.Type == "Z"]<-1
+df$prem.gas<- rep(0,length(df$Fuel.Type)) #Z
+df$prem.gas[df$Fuel.Type == "Z"]<-1
 
 ##TO EDIT CODE: character matching instead of listing values
 #vehicle class categorized into cars and trucks, 2 levels, 1 dummy variable
 #cars: two-seater, minicompact, subcompact, compact, mid-size, full-size, station wagon (small & mid-size)
 #trucks: pick-up truck(small & standard), SUV (small& standard), minivan, passenger van, special purpose vehicle 
-car.vehicle <- rep(0, length(df$Vehicle.Class))
+df$car.vehicle <- rep(0, length(df$Vehicle.Class))
 for (i in 1:length(df$Vehicle.Class))
   {
   if (df$Vehicle.Class[i] == "Two-seater" | df$Vehicle.Class[i] =="Minicompact" | df$Vehicle.Class[i] =="Subcompact" | 
       df$Vehicle.Class[i] =="Compact" | df$Vehicle.Class[i] =="Mid-size" | df$Vehicle.Class[i] =="Full-size" | 
       df$Vehicle.Class[i] =="Station wagon: Small" | df$Vehicle.Class[i] =="Station wagon: Mid-size")
     {
-    car.vehicle[i] <-1
+    df$car.vehicle[i] <-1
     }
 }
 
-#original level: 21; AS10, AS9, A8, AM7, AS8, AM8, M6, A9, AS6, A6, AV, M5, A10, A5, AV7, AM6, AS7, AV8, AV10, AV6, AS5 
-#tranmission categorized into AS, AV, AM, A, M- 5 levels; 4 dummy variables
-#manual as base level
-###loop for A-automatic
-autom<- rep(0, length(df$Transmission))
-for (i in 1:length(df$Transmission))
-  {
-  if (df$Transmission[i] == "A8" | df$Transmission[i] =="A9" | df$Transmission[i] =="A6" | 
-      df$Transmission[i] =="A10" | df$Transmission[i] =="A5")
-    {
-    autom[i] <-1
-    }
-}
 
-###loop for AM-automated manual
-auto.man<- rep(0, length(df$Transmission))
-for (i in 1:length(df$Transmission)){
-  if (df$Transmission[i] == "AM7" | df$Transmission[i] =="AM8" | df$Transmission[i] =="AM6")
-  {
-    auto.man[i] <-1
-  }
-}
-
-###loop for AS-automatic w/ select shift
-auto.shift<- rep(0, length(df$Transmission))
-for (i in 1:length(df$Transmission)){
-  if (df$Transmission[i] == "AS10" | df$Transmission[i] =="AS9" | df$Transmission[i] =="AS8" | 
-      df$Transmission[i] =="AS6" | df$Transmission[i] =="AS5" | df$Transmission[i] =="AS7")
-  {
-    auto.shift[i] <-1
-  }
-}
-
-###loop for AV-continuously variable
-av<- rep(0, length(df$Transmission))
-for (i in 1:length(df$Transmission)){
-  if (df$Transmission[i] == "AV" | df$Transmission[i] =="AV7" | df$Transmission[i] =="AV8" | 
-      df$Transmission[i] =="AV10" | df$Transmission[i] =="AV6")
-  {
-    av[i] <-1
-  }
-}
-
-
-#get all possible regression model by all-possible-regression selection 
-library(olsrr)
-fit.var.sel <- lm(co2.emissions~ Vehicle.Class+Transmission+engine.size+Fuel.Type+fuel.consumption.comb+Cylinders, data = df)
-summary(fit.var.sel)
-ols_step_all_possible(fit.var.sel)
-#lowest Cp-  Vehicle.Class, engine.size, Fuel.Type, fuel.consumption.comb
-
-
-#initial fitting of model only for fuel consumption comb, cylinders, engine size, fuel type, vehicle class, transmission (linear 1st order r/s)
-fit.first <- lm(co2.emissions~ fuel.consumption.comb+engine.size+Cylinders+ethanol+prem.gas+reg.gas+car.vehicle+autom+auto.man+auto.shift+av, data = df)
+#initial fitting of model only for fuel consumption comb, fuel type, vehicle class (linear 1st order r/s, no interaction)
+fit.first <- lm(co2.emissions~ fuel.consumption.comb+ethanol+prem.gas+reg.gas+car.vehicle, data = df)
 summary(fit.first)
 
 
-#residual of fit.interest
-res = residuals(fit.first)
-#plot each residuals
+#fit model with interaction terms: fuel consumption comb, fuel type and vehicle class
+fit.inter <- lm(co2.emissions~ fuel.consumption.comb+ethanol+prem.gas+reg.gas+car.vehicle+
+                  fuel.consumption.comb*ethanol+fuel.consumption.comb*prem.gas+fuel.consumption.comb*reg.gas+fuel.consumption.comb*car.vehicle+
+                  car.vehicle*ethanol+car.vehicle*prem.gas+car.vehicle*reg.gas+
+                  fuel.consumption.comb*ethanol*car.vehicle+fuel.consumption.comb*prem.gas*car.vehicle+fuel.consumption.comb*reg.gas*car.vehicle, data = df)
+summary(fit.inter)
+#colinearity present, to check car.vehicle and fuel type
+
+#use stepwise regression to decide which corelated independent variable(s) to drop
+#drop car. vehicle
+ols_step_both_p(fit.first)
+
+
+#fit model w/ interaction terms: fuel consumption and fuel type, fuel consumption and vehicle class; no interaction b/wn fuel type and vehicle class
+#removing colinearity
+fit.inter.reduced <- lm(co2.emissions~ fuel.consumption.comb+ethanol+prem.gas+reg.gas+car.vehicle+
+                          fuel.consumption.comb*ethanol+fuel.consumption.comb*prem.gas+fuel.consumption.comb*reg.gas+fuel.consumption.comb*car.vehicle, data = df)
+summary(fit.inter.reduced)
+
+
+#fit model w/ interaction terms: fuel consumption and fuel type
+fit.inter.reduced2 <- lm(co2.emissions~ fuel.consumption.comb+ethanol+prem.gas+reg.gas+
+                           fuel.consumption.comb*ethanol+fuel.consumption.comb*prem.gas+fuel.consumption.comb*reg.gas, data = df)
+summary(fit.inter.reduced2)
+
+#partial F-test w/ fit.inter.reduced gives p-value>0.05. Fail to reject Ho 
+#car.vehicle not significant; 
+#final model: fit.inter.reduced2
+anova(fit.inter.reduced, fit.inter.reduced2)
+
+
+##detecting lack of fit
+#residual plot: residuals vs fuel consumption 
+#no trend in plot, ~95% residuals within 2s of 0
+#sd=1.442
+res <- residuals(fit.inter.reduced2)
+plot(df$fuel.consumption.comb, res, ylab="Residuals", xlab="Fuel Consumption Comb", main="Residuals VS Fuel Consumption Comb")
+stan.resid[(stan.resid >2.884) | (stan.resid < -2.884)]
+
+##detecting lack of fit and unequal variances
+#residual plot: residuals vs predicted y
+#no trend in plot, ~95% residuals within 2s of 0, no transformation needed
+pred <- predict(fit.inter.reduced2)
+plot(pred, res, ylab="Residuals", xlab="Predicted CO2 Emission", main="Residuals VS Predicted CO2 Emission")
+
+#double-checked order of fuel consumption and goodnes of fit
+#straight line; statistically good fit
+termplot(fit.inter.reduced2, partial.resid = TRUE, ylab="Partial Residuals", xlab="Fuel Consumption Comb", main="Partial Residual Plot of Fuel Consumption Comb")
+
 #check normality
 qqnorm(res, ylab="Residuals", xlab="Normal Scores")
+#some outliers observed
 
+#identify outliers
+stan.resid <- rstandard(fit.inter.reduced2)
+plot(df$fuel.consumption.comb, stan.resid, ylab="Standardized Residuals", xlab="Fuel Consumption Comb", main="Standardized Residual Plot of Fuel Consumption Comb")
+abline(0,0)
+abline(3,0) #show regions w/ outliers
+abline(-3,0) #show regions w/ outliers
+stan.resid[(stan.resid >3) | (stan.resid < -3)]
 
+#create above graph highlighting outliers
+#decide what to do with outliers
+#figure indices of outliers in fuel consumption comb
+#change base level too avoid categorical multicolinearity
 
-xx
 
