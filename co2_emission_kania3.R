7af9bc100aaa4d456ccddb8d2381ae099eca1981
Kania Lywe <kanialywe@gmail.com>
2020-03-18 22:15:20 -0700

    left prediction and estimation

--- co2_emission_kania3.R
+++ co2_emission_kania3.R
@@ -61,6 +61,7 @@ ggplot(data = df, aes(x = fuel.consumption.comb, y = co2.emissions, color = car.
   geom_point() + 
   labs(x = "Fuel Consumption Comb (L/100km)", y = "CO2 Emissions (g/km)", title = "Cars (1) & Trucks (0)")
 
+
 #investigate engine size:
 ggplot(data = df, aes(x = fuel.consumption.comb, y = co2.emissions, color = engine.size)) + 
   geom_point() + 
@@ -75,6 +76,7 @@ ggplot(data = df, aes(x = fuel.consumption.comb, y = co2.emissions, color = Fuel
                      values = c('coral2', 'springgreen3', 'lightskyblue', 'plum4'),
                      breaks = c('D', 'E', 'X', 'Z'),
                      labels = c('Diesel', 'Ethanol', 'Regular Gasoline', 'Premium Gasoline'))
+
 
 #investigate cylinders
 ggplot(data = df, aes(x = fuel.consumption.comb, y = co2.emissions, color = Cylinders)) + 
@@ -93,7 +95,7 @@ df$reg.gas[df$Fuel.Type == "X"]<-1
 df$diesel<- rep(0,length(df$Fuel.Type)) #D
 df$diesel[df$Fuel.Type == "D"]<-1
 
-
+####--------------------------------------------Model building----------------------------------------------------------------------------------------------------
 #initial fitting of model- fuel consumption comb, fuel type, vehicle class (linear 1st order r/s, no interaction)
 fit.first <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel + Cylinders + engine.size, data = df)
 summary(fit.first)
@@ -119,6 +121,10 @@ fit.inter1 <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + dies
 summary(fit.inter1)
 #muticolinearity present
 
+#cv of model 1
+cv1 <- 1.444/mean(df$co2.emissions)*100
+
+
 #check if cylinder is significant
 fit.inter2 <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel + engine.size +
                    fuel.consumption.comb*ethanol + fuel.consumption.comb*reg.gas + fuel.consumption.comb*diesel +
@@ -133,6 +139,9 @@ anova(fit.inter1, fit.inter2)
 #Partial F-test p-value >0.05, fail to reject Ho. 
 #cylinder not significant
 
+#cv of model 2
+cv2 <- 1.433/mean(df$co2.emissions)*100
+
 
 #check if engine size is significant
 fit.inter3 <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel +
@@ -144,7 +153,11 @@ anova(fit.inter2, fit.inter3)
 #Partial F-test p-value >0.05, fail to reject Ho. 
 #engine size not significant
 
-#check significant of interaction terms
+#cv of model 3
+cv3 <- 1.442/mean(df$co2.emissions)*100 
+
+
+#check significance of interaction terms
 fit.inter4 <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel, data = df)
 
 summary(fit.inter4)
@@ -152,7 +165,10 @@ anova(fit.inter3, fit.inter4)
 #Partial F-test p-value < 0.05, reject Ho. 
 #interaction term is significant
 
+#cv of model 4
+cv4 <- 3.008/mean(df$co2.emissions)*100 
 
+####--------------------------------------------Residual analysis----------------------------------------------------------------------------------------------------
 ###Checking assumptions:
 ### 1. Lack of fit
 ### 2. Equal variances
@@ -236,71 +252,90 @@ ggplot(data = df, aes(x = fuel.consumption.comb, y = stan.resid)) +
   geom_abline(aes(intercept = -3, slope = 0, color = 'red')) +
   labs(x = "Fuel Consumption Comb (L/100km)", y = "Standardized Residuals", title = "Standardized Residuals VS Fuel Consumption (Comb)") +
   theme(legend.position = 'none') +
-  geom_text_repel(aes(label=ifelse((stan.resid > 3 |stan.resid < -3),round(res,4),"")), hjust=1.1)
+  geom_text_repel(aes(label=ifelse((stan.resid > 3 |stan.resid < -3),round(stan.resid,4),"")), hjust=1.1)
 
 
-#remove outliers; 10 points if use 2s from residual plot; 6 points if use 3 from stand. residual plot
-df.removed <- df[-c(44, 46, 78, 83, 93, 106, 119, 136, 147, 197),]
-df.removed <- df[-c(44, 46, 78, 93, 119, 136, 197),]
+#remove outliers; 6 points
+df.removed <- df[-c(44, 46, 93, 119, 136, 197),]
 
 #double check if really removed
 length(df$fuel.consumption.comb)
 length(df.removed$fuel.consumption.comb)
 
-###--------------------------------------------------Re-fit models etc; don't know if redundant------------------------------------###
-
+####--------------------------------------------Re-fit model without outliers----------------------------------------------------------------------------------------------------
 #initial fitting of model- fuel consumption comb, fuel type, vehicle class (linear 1st order r/s, no interaction)
-fit.first.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + prem.gas + reg.gas + car.vehicle, data = df.removed)
+fit.first.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel + Cylinders + engine.size, data = df.removed)
 summary(fit.first.removed)
+car::vif(fit.first.removed)
+
+#fit model with interaction terms: fuel consumption, fuel type, cylinder, engine size
+fit.inter1.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel + Cylinders + engine.size +
+                   fuel.consumption.comb*ethanol + fuel.consumption.comb*reg.gas + fuel.consumption.comb*diesel +
+                   fuel.consumption.comb*Cylinders + fuel.consumption.comb*engine.size + 
+                   Cylinders*ethanol + Cylinders*reg.gas + Cylinders*diesel +
+                   engine.size*ethanol + engine.size*reg.gas + engine.size*diesel +
+                   Cylinders*engine.size +
+                   fuel.consumption.comb*Cylinders*ethanol +
+                   fuel.consumption.comb*Cylinders*reg.gas +
+                   fuel.consumption.comb*Cylinders*diesel +
+                   fuel.consumption.comb*engine.size*ethanol +
+                   fuel.consumption.comb*engine.size*reg.gas + 
+                   fuel.consumption.comb*engine.size*diesel +
+                   fuel.consumption.comb*Cylinders*engine.size +
+                   fuel.consumption.comb*ethanol*Cylinders*engine.size +
+                   fuel.consumption.comb*reg.gas*Cylinders*engine.size +
+                   fuel.consumption.comb*diesel*Cylinders*engine.size, data = df.removed)
+summary(fit.inter1.removed)
+#muticolinearity present
+
+#cv of model 1
+cv1.removed <- 1.165/mean(df.removed$co2.emissions)*100
 
 
-#fit model with interaction terms: fuel consumption comb, fuel type and vehicle class
-fit.inter.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + prem.gas + reg.gas + car.vehicle +
-                  fuel.consumption.comb*ethanol + fuel.consumption.comb*prem.gas + fuel.consumption.comb*reg.gas + 
-                  fuel.consumption.comb*car.vehicle + car.vehicle*ethanol + car.vehicle*prem.gas + car.vehicle*reg.gas +
-                  fuel.consumption.comb*ethanol*car.vehicle + fuel.consumption.comb*prem.gas*car.vehicle + 
-                  fuel.consumption.comb*reg.gas*car.vehicle, data = df.removed)
+#check if cylinder is significant
+fit.inter2.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel + engine.size +
+                   fuel.consumption.comb*ethanol + fuel.consumption.comb*reg.gas + fuel.consumption.comb*diesel +
+                   fuel.consumption.comb*engine.size + 
+                   engine.size*ethanol + engine.size*reg.gas + engine.size*diesel +
+                   fuel.consumption.comb*engine.size*ethanol +
+                   fuel.consumption.comb*engine.size*reg.gas + 
+                   fuel.consumption.comb*engine.size*diesel, data = df.removed)
 
-summary(fit.inter.removed)
-#colinearity present; check car.vehicle and fuel type
+summary(fit.inter2.removed)
+anova(fit.inter1.removed, fit.inter2.removed)
+#Partial F-test p-value >0.05, fail to reject Ho. 
+#cylinder not significant
 
-
-#use stepwise regression to decide which corelated independent variable(s) to drop
-#results: drop car.vehicle
-ols_step_both_p(fit.first.removed)
-
-
-#fit model w/ interaction terms: fuel consumption and fuel type, fuel consumption and vehicle class
-#no interaction b/wn fuel type and vehicle class
-#goal: remove colinearity
-fit.inter.reduced.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol+prem.gas + reg.gas + car.vehicle +
-                          fuel.consumption.comb*ethanol + fuel.consumption.comb*prem.gas + fuel.consumption.comb*reg.gas + 
-                          fuel.consumption.comb*car.vehicle, data = df.removed)
-
-summary(fit.inter.reduced.removed)
+#cv of model 2
+cv2.removed <- 1.169/mean(df.removed$co2.emissions)*100
 
 
-#fit model w/ interaction terms: fuel consumption and fuel type
-fit.inter.reduced2.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + prem.gas + reg.gas +
-                           fuel.consumption.comb*ethanol + fuel.consumption.comb*prem.gas + fuel.consumption.comb*reg.gas, 
-                           data = df.removed)
+#check if engine size is significant
+fit.inter3.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel +
+                   fuel.consumption.comb*ethanol + fuel.consumption.comb*reg.gas + 
+                   fuel.consumption.comb*diesel,  data = df.removed)
 
-summary(fit.inter.reduced2.removed)
+summary(fit.inter3.removed)
+anova(fit.inter2.removed, fit.inter3.removed)
+#Partial F-test p-value >0.05, fail to reject Ho. 
+#engine size not significant
 
-
-#partial F-test w/ fit.inter.reduced gives p-value>0.05. Fail to reject Ho 
-#car.vehicle not significant; 
-anova(fit.inter.reduced.removed, fit.inter.reduced2.removed)
-
-
-#check significance of interaction term
-fit.simple.removed <- lm(co2.emissions~ fuel.consumption.comb+ethanol+prem.gas+reg.gas, data = df.removed) 
-summary(fit.simple.removed)
-anova(fit.inter.reduced2.removed,fit.simple.removed)
-#p-value < 0.05, reject Ho. Interaction term is useful in model.
-#final model: fit.inter.reduced2
+#cv of model 3
+cv3.removed <- 1.183/mean(df.removed$co2.emissions)*100 
 
 
+#check significant of interaction terms
+fit.inter4.removed <- lm(co2.emissions~ fuel.consumption.comb + ethanol + reg.gas + diesel, data = df.removed)
+
+summary(fit.inter4.removed)
+anova(fit.inter3.removed, fit.inter4.removed)
+#Partial F-test p-value < 0.05, reject Ho. 
+#interaction term is significant
+
+#cv of model 4
+cv4.removed <- 2.927/mean(df.removed$co2.emissions)*100 
+
+####--------------------------------------------Residual analysis without outliers----------------------------------------------------------------------------------------------------
 ###Checking assumptions:
 ### 1. Lack of fit
 ### 2. Equal variances
@@ -309,50 +344,50 @@ anova(fit.inter.reduced2.removed,fit.simple.removed)
 ##detecting lack of fit
 #residual plot: residuals vs fuel consumption 
 #no trend in plot, ~95% residuals within 2s of 0
-#sd=1.103
-df.removed$res <- residuals(fit.inter.reduced2.removed)
-df.removed$res[(df.removed$res >2.206) | (df.removed$res < -2.206)] #outside of 2s -> 44,46,78,83,93,106,119,136,147,197
+#sd=1.183
+df.removed$res <- residuals(fit.inter3.removed)
+df.removed$res[(df.removed$res >2.366) | (df.removed$res < -2.366)] #outside of 2s -> 44,46,78,83,93,106,119,136,147,197
 
 #filter points outside of 2s from df
 highlight.df.removed <- df.removed %>% 
-  filter((df.removed$res >2.206) | (df.removed$res < -2.206))
+  filter((df.removed$res >2.366) | (df.removed$res < -2.366))
 
 #plot residuals, highlight outliers, draw abline to outline regions of outliers, 
 #create x&y labels and title, remove legend, add in outlier values
 ggplot(data = df.removed, aes(x = fuel.consumption.comb, y = res)) + 
   geom_point() +
   geom_point(data = highlight.df.removed, aes(x = fuel.consumption.comb, y = res), color = 'red') +
-  geom_abline(aes(intercept = 2.206, slope = 0), color = 'red') +
-  geom_abline(aes(intercept = -2.206, slope = 0, color = 'red')) +
+  geom_abline(aes(intercept = 2.366, slope = 0), color = 'red') +
+  geom_abline(aes(intercept = -2.366, slope = 0, color = 'red')) +
   labs(x = "Fuel Consumption Comb (L/100km)", y = "Residuals", title = "Residuals VS Fuel Consumption (Comb) Without Outliers") +
   theme(legend.position = 'none') +
-  geom_text_repel(aes(label=ifelse((res > 2.206 |res < -2.206),round(res,4),"")), hjust=1.1)
+  geom_text_repel(aes(label=ifelse((res > 2.366 |res < -2.366),round(res,4),"")), hjust=1.1)
 
 
 ##detecting lack of fit and unequal variances
 #residual plot: residuals vs predicted y
 #no trend in plot, ~95% residuals within 2s of 0, no transformation needed
-df.removed$pred <- predict(fit.inter.reduced2.removed)
+df.removed$pred <- predict(fit.inter3.removed)
 
 #filter points outside of 2s from df
 highlight.df.removed <- df.removed %>% 
-  filter((df.removed$res >2.206) | (df.removed$res < -2.206))
+  filter((df.removed$res >2.366) | (df.removed$res < -2.366))
 
 #plot residuals, highlight outliers, draw abline to outline regions of outliers, 
 #create x&y labels and title, remove legend, add in outlier values
 ggplot(data = df.removed, aes(x = pred, y = res)) + 
   geom_point() +
   geom_point(data = highlight.df.removed, aes(x = pred, y = res), color = 'red') +
-  geom_abline(aes(intercept = 2.206, slope = 0), color = 'red') +
-  geom_abline(aes(intercept = -2.206, slope = 0, color = 'red')) +
+  geom_abline(aes(intercept = 2.366, slope = 0), color = 'red') +
+  geom_abline(aes(intercept = -2.366, slope = 0, color = 'red')) +
   labs(x = "Predicted CO2 Emissions (g/km)", y = "Residuals", title = "Residuals VS Predicted CO2 Emissions Without Outliers") +
   theme(legend.position = 'none') +
-  geom_text_repel(aes(label=ifelse((res > 2.206 |res < -2.206),round(res,4),"")), hjust=1.1)
+  geom_text_repel(aes(label=ifelse((res > 2.366 |res < -2.366),round(res,4),"")), hjust=1.1)
 
 
 ##double-checked order of fuel consumption and goodnes of fit
 #straight line; statistically good fit
-termplot(fit.inter.reduced2.removed, partial.resid = TRUE, 
+termplot(fit.inter3.removed, partial.resid = TRUE, 
          ylab="Partial Residuals", xlab="Fuel Consumption Comb (L/100km)", 
          main="Partial Residual Plot of Fuel Consumption (Comb) Without Outliers")
 
@@ -367,7 +402,7 @@ ggplot(data = df.removed) +
 
 
 ##identify outliers
-df.removed$stan.resid <- rstandard(fit.inter.reduced2.removed)
+df.removed$stan.resid <- rstandard(fit.inter3.removed)
 df.removed$stan.resid[(df.removed$stan.resid >3) | (df.removed$stan.resid < -3)]
 
 #filter points outside of 2s from df which has pred variable
@@ -383,18 +418,9 @@ ggplot(data = df.removed, aes(x = fuel.consumption.comb, y = stan.resid)) +
   geom_abline(aes(intercept = -3, slope = 0, color = 'red')) +
   labs(x = "Fuel Consumption Comb (L/100km)", y = "Standardized Residuals", title = "Standardized Residuals VS Fuel Consumption (Comb) Without Outliers") +
   theme(legend.position = 'none') +
-  geom_text_repel(aes(label=ifelse((stan.resid > 3 |stan.resid < -3),round(res,4),"")), hjust=1.1)
+  geom_text_repel(aes(label=ifelse((stan.resid > 3 |stan.resid < -3),round(stan.resid,4),"")), hjust=1.1)
 
 
-###------------------------------------------------------------------------------------------------------------------------------------------------##    
-
-## if no need to re-fit model to deleted data, just run this line           
-#replot 
-plot(df.removed$fuel.consumption.comb, stan.resid.removed, ylab="Standardized Residuals", xlab="Fuel Consumption Comb", main="Standardized Residual Plot of Fuel Consumption Comb")
-abline(0,0)
-abline(3,0) #show regions w/ outliers
-abline(-3,0) #show regions w/ outliers
-stan.resid.removed[(stan.resid.removed >3) | (stan.resid.removed < -3)]
-
+####--------------------------------------------Using final model for prection and estimation----------------------------------------------------------------------------------------------------
 
 
